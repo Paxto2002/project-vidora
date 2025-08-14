d081cc3b7e9c75619ff3f18cc28910bed11ef823
Paxto2002 <hafizwaseemahmed2002@gmail.com>
2025-08-14 18:53:25 +0500

    Custom api response and error handling

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import expres from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = expres();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Parse URL-encoded bodies
+app.use(express.static("public")); // Parse cookies
+app.use(cookieParser()); // Parse cookies
+export default app;
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,13 @@
+class apiResponse {
+  constructor(
+    statusCode,
+    data,
+    message = "Request was successful",
+    success = true
+  ) {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode >= 200 && statusCode < 400 ? success : false;
+  }
+}
